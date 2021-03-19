6767e95182b8e7625760dc955cfbe8aef08f9afc
unknown <nandiniswaraj13@gmail.com>
2021-03-19 14:36:06 +0530

    [Nandini swaraj]Added Edit  one record

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -8,11 +8,15 @@ namespace AddressBookSystem
        public static  Dictionary<string, Contact> addressBookStore = new Dictionary<string, Contact>();
         static void Main(string[] args)
         {
+
             Console.WriteLine("Wellcome To Address Book System Program!");
             Console.WriteLine("*****************************************");
             Console.WriteLine("Enter the book name you want to store");
             string bookName = Console.ReadLine();
             AddressBook.addBook(bookName);
+            Console.WriteLine("Enter the book name in which you want to Edit data:");
+            string bookNameHasReocrd = Console.ReadLine();
+            AddressBook.edit(bookNameHasReocrd);
         }
     }
 }
