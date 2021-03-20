7a6ddb5fa418e8985f2fdb517f910f1ab077c2e6
unknown <nandiniswaraj13@gmail.com>
2021-03-20 20:54:53 +0530

    [Nandini swaraj]Added Add new contact

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -5,15 +5,11 @@ namespace AddressBookSystem
 {
    public  class Program
     {
-       public static  Dictionary<string, List<Contact>> addressBookStore = new Dictionary<string, List<Contact>>();
         static void Main(string[] args)
         {
 
             Console.WriteLine("Wellcome To Address Book System Program!");
             Console.WriteLine("*****************************************");
-            Console.WriteLine("Enter the book name you want to store");
-            string bookName = Console.ReadLine();
-            AddressBook.addBook(bookName);
-          }
+           }
     }
 }
