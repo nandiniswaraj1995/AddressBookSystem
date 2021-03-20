5b6d3c67afbad84c513a3ff312427cb795d610b3
unknown <nandiniswaraj13@gmail.com>
2021-03-20 20:46:15 +0530

    [Nandini swaraj]Added Add new contact

--- AddressBookSystem/AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBookSystem/AddressBook.cs
@@ -8,8 +8,11 @@ namespace AddressBookSystem
     {
         public static void addBook(string bookName)
         {
+            if(!Program.addressBookStore.ContainsKey(bookName))
+            {
+                Program.addressBookStore.Add(bookName, new List<Contact>());
+            }
             Contact person = new Contact();
-
             Console.WriteLine("Enter All Details Like: ");
             Console.WriteLine("Enter First_Name: ");
             person.first_name = Console.ReadLine();
@@ -28,100 +31,13 @@ namespace AddressBookSystem
             Console.WriteLine("Enter Email:");
             person.email = Console.ReadLine();
 
-            Program.addressBookStore.Add(bookName, person);
+            List<Contact> book = new List<Contact>();
+            book.Add(person);
             Console.WriteLine("***************************************");
             Console.WriteLine("Your Record Added To :["+bookName+" Book]");
             Console.WriteLine(person.toString());
          
-        }
-        public static void edit(string bookName)
-        {
-            if(Program.addressBookStore.ContainsKey(bookName))
-            {
-                Contact record = Program.addressBookStore[bookName];
-                Console.WriteLine("Enter Person'First Name to modify data:");
-                string person_first_name = Console.ReadLine();
-
-                if (record.first_name.Equals(person_first_name))
-                {
-                    Console.WriteLine("Select Which Data You Want To Update \n1.First_Name \n2.Last_Name \n3.Address" +
-                       "\n4.City \n5.State \n6.Zip \n7.PhoneNumber \n8.Email");
-                    string selection = Console.ReadLine();
-                    string newData;
-                    switch (selection)
-                    {
-                        case "1":
-                            Console.WriteLine("Enter First_Name");
-                            newData = Console.ReadLine();
-                            record.first_name = newData;
-                            Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
-                            break;
-                        case "2":
-                            Console.WriteLine("Enter Last_Name");
-                            newData = Console.ReadLine();
-                            record.last_name = newData;
-                            Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
-                            break;
-                        case "3":
-                            Console.WriteLine("Enter Address");
-                            newData = Console.ReadLine();
-                            record.address = newData;
-                            Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
-                            break;
-                        case "4":
-                            Console.WriteLine("Enter City");
-                            newData = Console.ReadLine();
-                            record.city = newData;
-                            Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
-                            break;
-                        case "5":
-                            Console.WriteLine("Enter State");
-                            newData = Console.ReadLine();
-                            record.state = newData;
-                            Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
-                            break;
-                        case "6":
-                            Console.WriteLine("Enter Zip");
-                            newData = Console.ReadLine();
-                            record.zip = newData;
-                            Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
-                            break;
-                        case "7":
-                            Console.WriteLine("Enter PhoneNumber");
-                            newData = Console.ReadLine();
-                            record.phone_number = newData;
-                            Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
-                            break;
-                        case "8":
-                            Console.WriteLine("Enter Email");
-                            newData = Console.ReadLine();
-                            record.email = newData;
-                            Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
-                            break;
-                        default:
-                            Console.WriteLine("Invalid Selection Input");
-                            break;
-                    }
-
-
-                }
-                else
-                {
-                    Console.WriteLine("Person not found!");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Book not found!");
-            }
+        
         }
     }
 }
--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -5,7 +5,7 @@ namespace AddressBookSystem
 {
    public  class Program
     {
-       public static  Dictionary<string, Contact> addressBookStore = new Dictionary<string, Contact>();
+       public static  Dictionary<string, List<Contact>> addressBookStore = new Dictionary<string, List<Contact>>();
         static void Main(string[] args)
         {
 
@@ -14,9 +14,6 @@ namespace AddressBookSystem
             Console.WriteLine("Enter the book name you want to store");
             string bookName = Console.ReadLine();
             AddressBook.addBook(bookName);
-            Console.WriteLine("Enter the book name in which you want to Edit data:");
-            string bookNameHasReocrd = Console.ReadLine();
-            AddressBook.edit(bookNameHasReocrd);
-        }
+          }
     }
 }
