737ffa3e3d3db59317301924d124d92d023a3133
unknown <nandiniswaraj13@gmail.com>
2021-03-20 20:59:15 +0530

    [Nandini swaraj]Added Add new contact

---
+++ AddressBookSystem/AddressBookSystem/AddressBook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+        public static void addBook(string bookName)
+        {
+            if (!Program.addressBookStore.ContainsKey(bookName))
+            {
+                Program.addressBookStore.Add(bookName, new List<Contact>());
+            }
+            Contact person = new Contact();
+            Console.WriteLine("Enter All Details Like: ");
+            Console.WriteLine("Enter First_Name: ");
+            person.first_name = Console.ReadLine();
+            Console.WriteLine("Enter Last Name:");
+            person.last_name = Console.ReadLine();
+            Console.WriteLine("Enter Addree :");
+            person.address = Console.ReadLine();
+            Console.WriteLine("Enter City Name:");
+            person.city = Console.ReadLine();
+            Console.WriteLine("Enter State Name:");
+            person.state = Console.ReadLine();
+            Console.WriteLine("Enter pin Number:");
+            person.zip = Console.ReadLine();
+            Console.WriteLine("Enter Phone Number:");
+            person.phone_number = Console.ReadLine();
+            Console.WriteLine("Enter Email:");
+            person.email = Console.ReadLine();
+
+            List<Contact> book = new List<Contact>();
+            book.Add(person);
+            Console.WriteLine("***************************************");
+            Console.WriteLine("Your Record Added To :[" + bookName + " Book]");
+            Console.WriteLine(person.toString());
+
+
+        }
+    }
+}
+
+    
+
--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -5,11 +5,18 @@ namespace AddressBookSystem
 {
    public  class Program
     {
+        public static Dictionary<string, List<Contact>> addressBookStore = new Dictionary<string, List<Contact>>();
+
         static void Main(string[] args)
         {
 
             Console.WriteLine("Wellcome To Address Book System Program!");
             Console.WriteLine("*****************************************");
-           }
+            Console.WriteLine("Enter the book name you want to store");
+            string bookName = Console.ReadLine();
+            AddressBook.addBook(bookName);
+
+
+        }
     }
 }
